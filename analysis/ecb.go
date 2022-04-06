10deaaa81fd5cd5cfe93fd800c8a981869663746
Michael Senn <michael@morrolan.ch>
2022-04-06 13:21:32 +0200

    Implement PKCS#7-style padding and unpadding.

--- analysis/ecb.go
+++ analysis/ecb.go
@@ -4,6 +4,20 @@ const AesBlockSize = 16
 
 type AesBlock [AesBlockSize]byte
 
+// DetectEcb attempts to detect if the provided ciphertext is the result of AES
+// in ECB mode.
+//
+// It does so by checking if any two blocks of ciphertext are equal. As such
+// this detection method is not perfect.
+// There is a small chance of 2^(-128) that two random blocks of ciphertext are
+// equal without ECB being involved. As such there is a non-zero, albeit tiny,
+// chance that any random ciphertext consisting of n blocks will have one block
+// repeating, leading to a false-positive.
+// A bound for the false-positivity rate can be gotten by looking at
+// bounds for P(Binom(n, 2^(-128)) > 0 of the binomial distriubtion.
+//
+// Similarly there is a possibility of a false negative, which will happen if
+// the original plaintext message had no repeating blocks.
 func DetectEcb(ctxt []byte) bool {
 	// AES in ECB mode is guaranteed to produce block-aligned ciphertexts
 	if len(ctxt)%AesBlockSize != 0 {
---
+++ padding/pkcs7.go
@@ -0,0 +1,49 @@
+package padding
+
+import "fmt"
+
+const BlockSize = 16
+
+// PKCS7Pad pads the message according to PKCS#7 padding to a multiple of 16
+// bytes.
+//
+// It does so by adding x bytes of value x, where x is the number required to
+// get to the *next larger* multiple of 16. This means that a message with a
+// length which already is a multiple of 16 will be padded with 16 bytes of
+// value 0x10.
+func PKCS7Pad(msg []byte) []byte {
+	padBytes := BlockSize - (len(msg) % BlockSize)
+
+	padded := make([]byte, len(msg)+padBytes)
+	copy(padded, msg)
+	for i := len(msg); i < len(padded); i++ {
+		padded[i] = byte(padBytes)
+	}
+
+	return padded
+}
+
+// PKCS7Unpad removes PKCS#7-style padding from the supplied message.
+//
+// If the padding is invalid, an error is returned.
+func PKCS7Unpad(msg []byte) ([]byte, error) {
+	msgLength := len(msg)
+	padBytes := msg[msgLength-1]
+
+	// We'll verify the padding by checking that the last padBytes bytes
+	// are equal to padBytes.
+	for i := 0; i < int(padBytes); i++ {
+		if msg[msgLength-1-i] != padBytes {
+			return []byte{}, fmt.Errorf(
+				"Invalid padding in message, got %x byte, expected %x",
+				msg[msgLength-1-i],
+				padBytes,
+			)
+		}
+	}
+
+	unpadded := make([]byte, len(msg)-int(padBytes))
+	copy(unpadded, msg)
+
+	return unpadded, nil
+}
