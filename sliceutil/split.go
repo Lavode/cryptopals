9df14bee18b0b06839cd256d9d9c1ee73fb2152e
Michael Senn <michael@morrolan.ch>
2022-04-06 10:45:18 +0200

    Implement function to split slices using separator.

--- sliceutil/split.go
+++ sliceutil/split.go
@@ -15,3 +15,26 @@ func Alternating[T any](ts []T, n int) [][]T {
 
 	return out
 }
+
+// Split splits the provided slice by the given separator. The separators will
+// not be part of the output.
+//
+// If there are multiple separators in sequence, or at the beginning or end of
+// the slice, then the output will contain empty slices.
+//
+// If the separator is not present, the output will be a single slice.
+func Split[T comparable](ts []T, separator T) [][]T {
+	out := make([][]T, 0)
+	out = append(out, make([]T, 0))
+
+	for _, t := range ts {
+		if t == separator {
+			out = append(out, make([]T, 0))
+		} else {
+			lastIdx := len(out) - 1
+			out[lastIdx] = append(out[lastIdx], t)
+		}
+	}
+
+	return out
+}
--- sliceutil/split_test.go
+++ sliceutil/split_test.go
@@ -32,3 +32,57 @@ func ExampleAlternating() {
 	fmt.Println(Alternating([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 3))
 	// Output: [[1 4 7 10] [2 5 8] [3 6 9]]
 }
+
+func TestSplit(t *testing.T) {
+	in := []int{1, 2, 5, 3, 5, 7, 8, 9, 5, 5}
+	expect.Equals(
+		t,
+		[][]int{
+			[]int{1, 2},
+			[]int{3},
+			[]int{7, 8, 9},
+			[]int{},
+			[]int{},
+		},
+		Split(in, 5),
+	)
+
+	in2 := []byte{0x20, 0x10, 0x15, 0x20, 0x12}
+	expect.Equals(
+		t,
+		[][]byte{
+			[]byte{},
+			[]byte{0x10, 0x15},
+			[]byte{0x12},
+		},
+		Split(in2, 0x20),
+	)
+}
+
+func TestSplitEmptyInput(t *testing.T) {
+	in := []string{}
+	expect.Equals(
+		t,
+		[][]string{in},
+		Split(in, " "),
+	)
+}
+
+func TestSplitSeparatorNotPresent(t *testing.T) {
+	in := []int{1, 2, 3, 4}
+	expect.Equals(
+		t,
+		[][]int{in},
+		Split(in, 5),
+	)
+}
+
+func ExampleSplit() {
+	fmt.Println(Split([]int{1, 2, 3, 4, 5, 6, 7, 5, 9, 5}, 5))
+	// Output: [[1 2 3 4] [6 7] [9] []]
+}
+
+func ExampleSplit_noseparator() {
+	fmt.Println(Split([]int{1, 2, 3, 4, 5, 6, 7, 5, 9, 5}, 10))
+	// Output: [[1 2 3 4 5 6 7 5 9 5]]
+}
